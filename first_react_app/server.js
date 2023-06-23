d280ec6a513319977ff9c6bb93c5883582758520
BBricha <baderbricha@gmail.com>
2023-06-22 18:05:19 -0700

    First React App

---
+++ first_react_app/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const PORT = 4000;
+
+const app = express();
+ 
+app.use(express.static(path.join(__dirname, "./build")));
+
+
+app.listen(PORT, () => {
+    console.log("running on port", PORT)
+})
